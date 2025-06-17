728e18e0e660f14d147082f0833376fef0773772
Laptop <rubin3215@gmail.com>
2025-06-17 17:08:29 +0300

    Refactor: delete an unused page

--- project/scripts/navbar.js
+++ project/scripts/navbar.js
@@ -28,11 +28,11 @@ function checkAuth() {
 function setNavbarFromResponse(data) {
     const rightSide = document.querySelector('.right-side');
     const leftSide = document.querySelector('.left-side');
+
     rightSide.innerHTML = '';
     leftSide.innerHTML = `
         <a href="../pages/home.html">Главная</a>
         <a href="../pages/discussions.html">Обсуждения</a>
-<!--        <a href="../pages/blogs.html">Блоги</a>-->
         <a href="../pages/users.html">Пользователи</a>
     `;
 
