ae4b311e63ba9f797e952866711bd6680fc1b12d
Laptop <rubin3215@gmail.com>
2025-07-10 02:33:05 +0300

    Refactor: create const for ROLE_ADMIN value

--- src/assets/scripts/config.js
+++ src/assets/scripts/config.js
@@ -6,6 +6,8 @@ export const RECONNECT_DELAY = 3_000
 export const PRELOAD_AVATAR = '/avatars/preload.jpg'
 export const DEFAULT_AVATAR = '/avatars/default.jpg'
 
+export const ROLE_ADMIN = 'ROLE_ADMIN'
+
 const DEV_CONFIG = {
     protocol: 'http',
     domain: 'localhost',
